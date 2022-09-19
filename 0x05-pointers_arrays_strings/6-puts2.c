338f45a470f697b53ec17b1da5cef84c002c13f9
emuiga <muigastephen14@gmail.com>
2022-09-19 13:41:45 -0700

    Add file 7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - function that prints every other character starting with the first
+ * @str: the input
+ * return: print out
+ */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+		{
+			putchar(str[count]);
+			count++;
+		}
+	}
+}
