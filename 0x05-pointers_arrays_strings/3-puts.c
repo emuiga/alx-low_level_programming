06be771fc95eb9aa714b29d927484dabf049edf9
emuiga <muigastephen14@gmail.com>
2022-09-19 13:09:47 -0700

    Add file 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - function prints a string to stdout
+ * @str: the value being considered
+ * return: not
+ */
+void _puts(char *str)
+{
+	int l = 0;
+
+	while (*str + l) != '\0')
+	{
+		putchar(str[l]);
+		l++;
+	}
+	putchar('\n');
+}
